d686a7936f047c50f3f1d73fa0140bb1c28100a6
Aoibhe Wilson <aoibhe@wilsons.io>
2023-09-01 22:23:51 +0200

    feat(i18n): Add Language Switch and Theme Switch
    - Provides translation files
    - Provides utilities for language
    - Toggles between light and dark modes

---
+++ apps/web/src/i18n/ui/en.ts
@@ -0,0 +1,8 @@
+export const EN = {
+    'nav.home': 'Home',
+    'nav.blog': 'Blog',
+    'nav.cv': 'CV',
+    'util.hello': '*Hello*\n\nwhat now?',
+    'lang.viewInJP': 'このページを日本語でご覧になりたい方は',
+    'lang.viewInEN': 'View this page in English',
+}
---
+++ apps/web/src/i18n/ui/index.ts
@@ -0,0 +1,14 @@
+import { EN } from './en';
+import { JP } from './jp';
+
+export const languages = {
+    en: 'English',
+    jp: '日本語',
+}
+
+export const defaultLang = 'en';
+
+export const ui = {
+    en: EN,
+    jp: JP,
+}
