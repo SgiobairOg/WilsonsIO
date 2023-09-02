1b2a200c26209251d997726a087f59dfb04b9a6c
Aoibhe Wilson <aoibhe@wilsons.io>
2023-09-02 15:25:14 +0200

    chore(i18n): Add translations

--- apps/web/src/i18n/ui/en.ts
+++ apps/web/src/i18n/ui/en.ts
@@ -2,7 +2,11 @@ export const EN = {
     'nav.home': 'Home',
     'nav.blog': 'Blog',
     'nav.cv': 'CV',
-    'util.hello': '*Hello*\n\nwhat now?',
+    'nav.about': 'About',
+    'header.greeting': 'Aoibhe,\nat your\nservice',
+    'header.intro': `Designer /  Developer /  Seafarer /  Scientist /  Analyst`,
     'lang.viewInJP': 'このページを日本語でご覧になりたい方は',
     'lang.viewInEN': 'View this page in English',
+    'blog.title': 'Blog',
+    'blog.intro': `As I've begun to treat and recover from my ADD and burnout, I've started to write some more.\nI hope to make this a space where I can share useful information for you, and show off some of my abilities.`
 }
