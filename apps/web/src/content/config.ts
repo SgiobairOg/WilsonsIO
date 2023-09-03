83275ead172c7ea24f11bde2592ebff38c8bccec
Aoibhe Wilson <aoibhe@wilsons.io>
2023-09-03 15:52:34 +0200

    chore(web:blog): Update Astro Content Collection Config

--- apps/web/src/content/config.ts
+++ apps/web/src/content/config.ts
@@ -1,11 +1,17 @@
 import { defineCollection, z } from 'astro:content';
-import { employmentHistorySchema } from '../schemas';
+import { employmentHistorySchema, blogPostSchema } from '../schemas';
 
 const employmentHistoryCollection = defineCollection({
     type: 'content',
     schema: employmentHistorySchema,
 })
 
+const blogPostCollection = defineCollection({
+  type: 'content',
+  schema: blogPostSchema,
+})
+
 export const collections = {
-    'employmentHistory': employmentHistoryCollection
+    'employmentHistory': employmentHistoryCollection,
+    'blogPosts': blogPostCollection
 }
