9c42eb0f6a67625b6a6c551e6a6d06fef4860a79
Aoibhe Wilson <aoibhe@wilsons.io>
2023-09-06 20:33:30 +0200

    feat(devops): Add Middleware Typings

--- apps/web/src/env.d.ts
+++ apps/web/src/env.d.ts
@@ -1,2 +1,12 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+
+declare namespace App {
+  interface Locals {
+      transl: (key: TranslationLabel, options?: TranslationOptions) => string,
+      currentLang: string,
+      langPath: string,
+      logger: Logger,
+  }
+}
